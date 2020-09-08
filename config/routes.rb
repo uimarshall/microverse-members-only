36af607899076ccd00cf7cb043cf72d36ce82b23
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-09-08 11:53:37 +0300

    Overwrite devise registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
   resources :posts, only: %i[new create index]
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
